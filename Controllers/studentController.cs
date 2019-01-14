c1987c67657ba43f75691a757eb78d81597f8a72
bssm1989 <bssm1989@gamil.com>
2019-01-14 19:11:28 +0700

    connect  data santisart database serv  and show list

---
+++ Controllers/studentController.cs
@@ -0,0 +1,20 @@
+﻿using santisart_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace santisart_app.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        santisar_serv db = new santisar_serv();
+        public ActionResult Index()
+        {
+
+            return View(db.STUDENTS.ToList());
+        }
+    }
+}
