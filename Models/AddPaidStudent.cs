3d3bc718892af13605bb214b3acabedb7761f531
bssm1989 <bssm1989@gamil.com>
2019-01-17 13:24:28 +0700

    12121

---
+++ Models/AddPaidStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace santisart_app.Models
+{
+    public class AddPaidStudent
+    {
+        public int StudentId { get; set; }
+        public int[] MonthIdAndPaid { get; set; }
+        public int numPaid { get; set; }
+
+    }
+}
