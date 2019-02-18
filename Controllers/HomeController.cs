5c0f0c5dc58f90e5ec993f51903bb27f82d16525
bssm1989 <bssm1989@gamil.com>
2019-02-19 00:40:14 +0700

    excel3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,40 +12,44 @@ namespace santisart_app.Controllers
 
         public ActionResult Index()
         {
-          
-            string topLeft = "A1";
-            string bottomRight = "A4";
-            string graphTitle = "Graph Title";
-            string xAxis = "Time";
-            string yAxis = "Value";
-            var application = new Microsoft.Office.Interop.Excel.Application();
-            string pathExcel = Server.MapPath("ExcelTemp\\Book5.xlsx");// + V;
-           // string fileName = application.StartupPath + @"\ExcelTemp\Test.xlsm";//C:\Users\mumee\source\repos\santisart_app\santisart_app\ExcelTemp\Test.xlsx "\\ExcelTemp\\Test.xlsx";
-            var workbook = application.Workbooks.Open(pathExcel);
-            var worksheet = workbook.Worksheets[1] as
-                Microsoft.Office.Interop.Excel.Worksheet;
 
-            // Add chart.
-            var charts = worksheet.ChartObjects() as
-                Microsoft.Office.Interop.Excel.ChartObjects;
-            var chartObject = charts.Add(60, 10, 300, 300) as
-                Microsoft.Office.Interop.Excel.ChartObject;
-            var chart = chartObject.Chart;
+            // string topLeft = "A1";
+            // string bottomRight = "A4";
+            // string graphTitle = "Graph Title";
+            // string xAxis = "Time";
+            // string yAxis = "Value";
+            // var application = new Microsoft.Office.Interop.Excel.Application();
+            // string pathExcel = Server.MapPath("ExcelTemp\\Book5.xlsx");// + V;
+            //// string fileName = application.StartupPath + @"\ExcelTemp\Test.xlsm";//C:\Users\mumee\source\repos\santisart_app\santisart_app\ExcelTemp\Test.xlsx "\\ExcelTemp\\Test.xlsx";
+            // var workbook = application.Workbooks.Open(pathExcel);
+            // var worksheet = workbook.Worksheets[1] as
+            //     Microsoft.Office.Interop.Excel.Worksheet;
 
-            // Set chart range.
-            var range = worksheet.get_Range(topLeft, bottomRight);
-            chart.SetSourceData(range);
+            // // Add chart.
+            // var charts = worksheet.ChartObjects() as
+            //     Microsoft.Office.Interop.Excel.ChartObjects;
+            // var chartObject = charts.Add(60, 10, 300, 300) as
+            //     Microsoft.Office.Interop.Excel.ChartObject;
+            // var chart = chartObject.Chart;
 
-            // Set chart properties.
-            chart.ChartType = Microsoft.Office.Interop.Excel.XlChartType.xlLine;
-            chart.ChartWizard(Source: range,
-                Title: graphTitle,
-                CategoryTitle: xAxis,
-                ValueTitle: yAxis);
+            // // Set chart range.
+            // var range = worksheet.get_Range(topLeft, bottomRight);
+            // chart.SetSourceData(range);
 
-            //// Save.
-            workbook.Save();
-            workbook.Close();
+            // // Set chart properties.
+            // chart.ChartType = Microsoft.Office.Interop.Excel.XlChartType.xlLine;
+            // chart.ChartWizard(Source: range,
+            //     Title: graphTitle,
+            //     CategoryTitle: xAxis,
+            //     ValueTitle: yAxis);
+
+            // //// Save.
+            // workbook.Save();
+            // workbook.Close();
+            FileStream fs1 = new FileStream(Server.MapPath("ExcelTemp\\test.txt"), FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fs1);
+            writer.Write(Server.MapPath("ExcelTemp\\test.txt"));
+            writer.Close();
             return View();
         }
 
