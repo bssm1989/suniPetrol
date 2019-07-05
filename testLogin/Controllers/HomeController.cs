556029429b22c4099f6952ed0fbdaae0aad63e86
bssm1989 <bssm1989@gamil.com>
2019-07-05 07:18:55 +0700

    1

---
+++ testLogin/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testLogin.Models;
+
+namespace testLogin.Controllers
+{
+ 
+    public class HomeController : Controller
+    {
+        private santisar_Entities db = new santisar_Entities();
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
